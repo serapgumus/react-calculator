aee67e6fe76707ce10240e7b3fedcc2136dbcfdc
Serap Gumus <serapkgumus@gmail.com>
2020-10-05 00:07:54 +0300

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
+import calculator from "./calculator.js"
+
+const InputButton = (props) => (
+  <button value={props.value} className={`${props.value === "0" ? "col-span-2" : ""} bg-blue-400 hover:bg-blue-700 text-white text-xl font-bold py-2 px-4 rounded-full`} onClick={props.onClick}>{props.value}</button>)
+
+const OperationButton = (props) => (
+  <button value={props.op} className={"bg-orange-500 hover:bg-white hover:text-orange-500 text-white text-xl font-bold py-2 px-4 rounded-full"} onClick={props.onClick}>{props.op}</button>)
+
+const ParanButton = (props) => (
+  <button value={props.value} className={"bg-blue-200 hover:bg-blue-600 hover:text-blue-200 text-blue-700 text-xl font-bold py-2 px-4 rounded-full"} onClick={props.onClick}>{props.value}</button>)
+
+const Screen = (props) => <div className="grid grid-flow-row justify-center grid-cols-4 grid-rows-1 gap-1 px-2 py-2 ">
+  <div className="col-span-4 text-white text-4xl text-right">{props.value}</div>
+</div>
+
+const init = { count: "0", initialized: true, header: "Simple Calculator" };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'insert':
+      return { ...state, count: state.initialized ? action.payload : state.count + action.payload, initialized: false }
+    case 'calculate':
+      return { ...state, count: calculator(state.count) + "", initialized: true }
+    case 'reset':
+      return { ...state, count: "0", initialized: true }
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, init);
+  const valueInserted = (e) => dispatch({ type: 'insert', payload: e.target.value })
+  const calculateClicked = () => dispatch({ type: 'calculate' })
+  const reset = () => dispatch({ type: 'reset' })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen lg:mx-auto flex justify-center items-start bg-gray-800" >
+      <div className="container max-w-lg min-h-s bg-gray-800" >
+        <div className="text-4xl text-center p-8 text-blue-300">{state.header}</div>
+        <div className=" bg-gray-800  rounded px-8 pb-8">
+          <Screen value={state.count} />
+          <div className="grid grid-flow-row justify-center grid-cols-4 grid-rows-3 gap-1 px-2 py-2 ">
+            <ParanButton value="AC" onClick={reset} />
+            <ParanButton value="(" onClick={valueInserted} />
+            <ParanButton value=")" onClick={valueInserted} />
+            <OperationButton op="/" onClick={valueInserted} />
+            <InputButton value="7" onClick={valueInserted} />
+            <InputButton value="8" onClick={valueInserted} />
+            <InputButton value="9" onClick={valueInserted} />
+            <OperationButton op="x" onClick={valueInserted} />
+            <InputButton value="4" onClick={valueInserted} />
+            <InputButton value="5" onClick={valueInserted} />
+            <InputButton value="6" onClick={valueInserted} />
+            <OperationButton op="-" onClick={valueInserted} />
+            <InputButton value="1" onClick={valueInserted} />
+            <InputButton value="2" onClick={valueInserted} />
+            <InputButton value="3" onClick={valueInserted} />
+            <OperationButton op="+" onClick={valueInserted} />
+            <InputButton value="0" onClick={valueInserted} />
+            <InputButton value="." onClick={valueInserted} />
+            <OperationButton op="=" onClick={calculateClicked} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/calculator.js
@@ -0,0 +1,74 @@
+export default (string) => {
+
+  const getOpArr = str => str.match(/(([0-9]+)|([()/x+-]))/g);
+  const operation = operator => arr => {
+    let i = arr.indexOf(operator);
+    let item;
+    switch (operator) {
+      case "/":
+        item = Number(arr[i - 1]) / Number(arr[i + 1]);
+        break;
+      case "x":
+        item = Number(arr[i - 1]) * Number(arr[i + 1]);
+        break;
+      case "+":
+        item = Number(arr[i - 1]) + Number(arr[i + 1]);
+        break;
+      case "-":
+        item = Number(arr[i - 1]) - Number(arr[i + 1]);
+        break;
+      default:
+        throw new Error('Operator is not defined!');
+    }
+    arr.splice(i - 1, 3, item)
+    return arr
+  }
+
+  const division = operation("/")
+  const multiplication = operation("x")
+  const addition = operation("+")
+  const subtraction = operation("-")
+
+  const calculateWithParanthesis = arr => {
+    let openParanId = arr.indexOf('(');
+    let closeParanId = 0;
+    let paranNumber = 1;
+    for (let i = openParanId + 1; i < arr.length; i++) {
+      if (arr[i] === '(') paranNumber += 1;
+      if (arr[i] === ')') paranNumber -= 1;
+      if (paranNumber === 0) {
+        closeParanId = i;
+        break;
+      }
+    }
+    let item = calculate(arr.slice(openParanId + 1, closeParanId))
+    arr.splice(openParanId, closeParanId - openParanId + 1, item)
+    return arr
+  }
+
+  const calculate = (arr) => {
+    if (arr.length === 1) {
+      return Number(arr[0])
+    }
+    if (arr.includes('(')) {
+      return calculate(calculateWithParanthesis(arr))
+    }
+    else if (arr.includes('/') || arr.includes('x')) {
+      let divId = arr.indexOf('/');
+      let mulId = arr.indexOf('x');
+      if (divId > 0 && (mulId < 0 || divId < mulId)) return calculate(division(arr))
+      else return calculate(multiplication(arr))
+    }
+    else if (arr.includes('+') || arr.includes('-')) {
+      let addId = arr.indexOf('+');
+      let subId = arr.indexOf('-');
+      if (addId > 0 && (subId < 0 || addId < subId)) return calculate(addition(arr))
+      else return calculate(subtraction(arr))
+    }
+    else
+      throw new Error('Unexpected operation!');
+  }
+
+  return calculate(getOpArr(string))
+
+};
